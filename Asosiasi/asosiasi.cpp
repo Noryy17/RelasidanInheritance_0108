771d628519dff125e1cc3f02c2f693974e189035
Faid Arya Pamungkas <faid.arya.ft24@mail.umy.ac.id>
2025-05-27 20:22:25 +0700

    membuat class Dokter,untuk

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -21,3 +21,17 @@ class pasien{
 
 };
 
+class dokter {
+    public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) : nama(pNama){
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
